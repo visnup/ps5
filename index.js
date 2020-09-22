7d8748cf8243bd783659cc264c6b4ee3ceca0c9c
Visnu Pitiyanuvath <visnup@gmail.com>
2020-09-21 20:08:38 -0700

    Handle multiple results

--- index.js
+++ index.js
@@ -5,43 +5,42 @@ const twilio = Twilio(process.env.TWILIO_SID, process.env.TWILIO_AUTH);
 const sites = [
   {
     url: "https://www.amazon.com/dp/B08FC6MR62",
-    selector: "#availability",
-    text: "unavailable",
+    content: {"#availability": "unavailable"},
   },
   {
     url:
       "https://www.bestbuy.com/site/sony-playstation-5-digital-edition-console/6430161.p?skuId=6430161",
-    selector: ".fulfillment-add-to-cart-button",
-    text: "coming soon",
+    content: {".fulfillment-add-to-cart-button": "coming soon"},
   },
   {
     url:
       "https://www.target.com/p/playstation-5-digital-edition-console/-/A-81114596",
-    selector: "[data-test='preorderFulfillment']",
-    text: "sold out",
+    content: {"[data-test='preorderFulfillment']": "sold out"},
   },
   {
     url:
       "https://www.gamestop.com/video-games/playstation-5/consoles/products/playstation-5-digital-edition/11108141.html",
-    selector: ".add-to-cart-buttons",
-    text: "not available",
+    content: {".add-to-cart-buttons": "not available"},
   },
   {
-    url: "https://www.walmart.com/ip/PlayStation5-Console/363472942",
-    selector: ".prod-ProductOffer-oosMsg",
-    text: "out of stock",
+    url:
+      "https://www.walmart.com/ip/Sony-PlayStation-5-Digital-Edition/493824815",
+    content: {
+      ".prod-ProductOffer-oosMsg": "out of stock",
+      ".error-page-message": "backorder",
+    },
   },
   {
     url:
       "https://www.bhphotovideo.com/c/product/1595084-REG/sony_3005719_playstation_5_digital_edition.html",
-    selector: "[data-selenium='stockInfo']",
-    text: "coming soon",
+    content: {"[data-selenium='stockInfo']": "coming soon"},
   },
   {
     url:
       "https://www.samsclub.com/p/ps5-pre-order/prod24980181?xid=plp_product_1_14",
-    selector: ".sc-pc-large-desktop-oos-card-out-of-stock-text",
-    text: "out of stock",
+    content: {
+      ".sc-pc-large-desktop-oos-card-out-of-stock-text": "out of stock",
+    },
   },
 ];
 
@@ -58,7 +57,7 @@ const sites = [
   else process.exit(1);
 })();
 
-async function check({url, selector, text}) {
+async function check({url, content}) {
   const browser = await puppeteer.launch({
     defaultViewport: {width: 1280, height: 900},
   });
@@ -69,12 +68,14 @@ async function check({url, selector, text}) {
       "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36"
     );
     await page.goto(url);
-    const element = await page.$(selector);
-    if (element) {
-      await element.screenshot({path: `${name}.png`});
-      const innerText = await element.getProperty("innerText");
-      if ((await innerText.jsonValue()).toLowerCase().includes(text))
-        return console.log(`🔴 ${name}: unavailable`), false;
+    for (const [selector, text] of Object.entries(content)) {
+      const element = await page.$(selector);
+      if (element) {
+        await element.screenshot({path: `${name}.png`});
+        const innerText = await element.getProperty("innerText");
+        if ((await innerText.jsonValue()).toLowerCase().includes(text))
+          return console.log(`🔴 ${name}: unavailable`), false;
+      }
     }
     await page.screenshot({path: `${name}.png`});
     console.log(`🟢 ${name}: available?!`);
