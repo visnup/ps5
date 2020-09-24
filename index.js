425923bc6cf6fb1db719ac0a8d0bd6c457b8743f
Visnu Pitiyanuvath <visnup@gmail.com>
2020-09-24 08:15:56 -0700

    Turn gamestop and target back on

--- index.js
+++ index.js
@@ -67,7 +67,7 @@ async function check({url, content}) {
     await page.setUserAgent(
       "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36"
     );
-    await page.goto(url);
+    await page.goto(url, {waitUntil: "networkidle2"});
     for (const [selector, text] of Object.entries(content)) {
       const element = await page.$(selector);
       if (element) {
