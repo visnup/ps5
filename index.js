4cb021a6010431747cb74f6ebdfb296245a88cb7
Visnu Pitiyanuvath <visnup@gmail.com>
2020-11-18 09:19:07 -0800

    Update index.js

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ const sites = [
       "https://www.walmart.com/ip/Sony-PlayStation-5-Digital-Edition/493824815",
     content: {
       ".prod-ProductOffer-oosMsg": "out of stock",
-      ".prod-blitz-copy-message": "restocking",
+      ".prod-blitz-copy-message": "while supplies last",
       ".error-page-message": "backorder",
     },
   },
