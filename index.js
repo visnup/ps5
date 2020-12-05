6c9ba90c1a4f8b40d83a341b510ede39d11a152b
Visnu Pitiyanuvath <visnup@gmail.com>
2020-12-04 22:48:34 -0800

    Updates and prettier

--- index.js
+++ index.js
@@ -5,22 +5,26 @@ const twilio = Twilio(process.env.TWILIO_SID, process.env.TWILIO_AUTH);
 const sites = [
   {
     url: "https://www.amazon.com/dp/B08FC6MR62",
-    content: {"#availability": "unavailable"},
+    content: { "#availability": "unavailable" },
   },
   {
     url:
       "https://www.bestbuy.com/site/sony-playstation-5-digital-edition-console/6430161.p?skuId=6430161",
-    content: {".fulfillment-add-to-cart-button": "sold out"},
+    content: { ".fulfillment-add-to-cart-button": "sold out" },
   },
   {
     url:
       "https://www.target.com/p/playstation-5-digital-edition-console/-/A-81114596",
-    content: {"[data-test='preorderFulfillment']": "sold out", "[data-test='flexible-fulfillment']": "isn't eligible for shipping"},
+    content: {
+      "[data-test='preorderFulfillment']": "sold out",
+      "[data-test='flexible-fulfillment']": "isn't eligible for shipping",
+      "[data-test='storeBlockNonBuyableMessages']": "out of stock",
+    },
   },
   {
     url:
       "https://www.gamestop.com/video-games/playstation-5/consoles/products/playstation-5-digital-edition/11108141.html",
-    content: {".add-to-cart-buttons": "not available"},
+    content: { ".add-to-cart-buttons": "not available" },
   },
   {
     url:
@@ -34,13 +38,20 @@ const sites = [
   {
     url:
       "https://www.bhphotovideo.com/c/product/1595084-REG/sony_3005719_playstation_5_digital_edition.html",
-    content: {"[data-selenium='stockInfo']": "coming soon"},
+    content: { "[data-selenium='stockInfo']": "coming soon" },
   },
   {
     url:
       "https://www.samsclub.com/p/ps5-pre-order/prod24980181?xid=plp_product_1_14",
     content: {
       ".sc-pc-large-desktop-oos-card-out-of-stock-text": "out of stock",
+    },
+  },
+  {
+    url:
+      "https://www.costco.com/sony-playstation-5-gaming-console-bundle.product.100691489.html",
+    content: {
+      "#productImageOverlay [title='Out of Stock']": "",
     },
   },
 ];
@@ -58,9 +69,9 @@ const sites = [
   else process.exit(1);
 })();
 
-async function check({url, content}) {
+async function check({ url, content }) {
   const browser = await puppeteer.launch({
-    defaultViewport: {width: 1280, height: 900},
+    defaultViewport: { width: 1280, height: 900 },
   });
   try {
     const [, name] = url.match(/([^.]+).com/);
@@ -68,17 +79,17 @@ async function check({url, content}) {
     await page.setUserAgent(
       "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36"
     );
-    await page.goto(url, {waitUntil: "networkidle2"});
+    await page.goto(url, { waitUntil: "networkidle2" });
     for (const [selector, text] of Object.entries(content)) {
       const element = await page.$(selector);
       if (element) {
-        await element.screenshot({path: `${name}.png`});
+        await element.screenshot({ path: `${name}.png` });
         const innerText = await element.getProperty("innerText");
         if ((await innerText.jsonValue()).toLowerCase().includes(text))
           return console.log(`🔴 ${name}: unavailable`), false;
       }
     }
-    await page.screenshot({path: `${name}.png`});
+    await page.screenshot({ path: `${name}.png` });
     console.log(`🟢 ${name}: available?!`);
     return true;
   } catch (e) {
